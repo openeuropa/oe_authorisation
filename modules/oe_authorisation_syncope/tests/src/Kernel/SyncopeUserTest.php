1d28b2346d96f9ddbcd2b016af3ec023baa9ab11
upchuk <danny@webomelette.com>
2018-09-27 15:14:40 +0200

    OPENEUROPA-1239: Minor test improvement.

--- modules/oe_authorisation_syncope/tests/src/Kernel/SyncopeUserTest.php
+++ modules/oe_authorisation_syncope/tests/src/Kernel/SyncopeUserTest.php
@@ -5,6 +5,7 @@ declare(strict_types = 1);
 namespace Drupal\Tests\oe_authorisation_syncope\Kernel;
 
 use Drupal\oe_authorisation_syncope\Syncope\SyncopeUser;
+use Drupal\user\UserInterface;
 
 /**
  * Tests the Syncope user mapping.
@@ -17,10 +18,7 @@ class SyncopeUserTest extends SyncopeTestBase {
    * Ensures the corresponding actions take place in Syncope.
    */
   public function testUserLifeCycle(): void {
-    /** @var \Drupal\user\UserInterface $user */
-    $user = $this->container->get('entity_type.manager')->getStorage('user')
-      ->create(['name' => 'jack']);
-    $user->save();
+    $user = $this->createUser('jack');
 
     // Assert we have the user in Syncope.
     $uuid = $user->get('syncope_uuid')->value;
@@ -34,6 +32,8 @@ class SyncopeUserTest extends SyncopeTestBase {
     $user->setUsername('john');
     $user->addRole('site_manager');
     $user->save();
+
+    // Assert the changes have taken effect.
     $syncope_user = $this->getClient()->getUser($uuid);
     $this->assertInstanceOf(SyncopeUser::class, $syncope_user);
     $this->assertEquals('john@sitea', $syncope_user->getName());
@@ -45,16 +45,40 @@ class SyncopeUserTest extends SyncopeTestBase {
 
     // Delete the user.
     $user->delete();
+    // @todo assert the user got deleted.
+  }
+
+  /**
+   * Tests that a user can already pre-exist in Syncope and the mapping works.
+   */
+  public function testUserAlreadyExistsInSyncope(): void {
+    // Pre-create a user in Syncope with the Site Manager role.
+    /** @var \Drupal\user\RoleInterface $role */
+    $role = $this->container->get('entity_type.manager')->getStorage('user_role')->load('site_manager');
+    $uuid = $this->container->get('oe_authorisation_syncope.role_mapper')->getRoleUuid($role);
+
+    $syncope_user = new SyncopeUser('', 'jack', [$uuid]);
+    $syncope_user = $this->getClient()->createUser($syncope_user);
+    $this->assertCount(1, $syncope_user->getGroups());
+
+    // Create a user in Drupal with the same name.
+    $user = $this->createUser('jack');
+
+    // Assert the user is mapped and still has its role.
+    $syncope_user_bis = $this->getClient()->getUser($user->get('syncope_uuid')->value);
+    $this->assertInstanceOf(SyncopeUser::class, $syncope_user_bis);
+    $this->assertEquals($syncope_user_bis->getUuid(), $syncope_user->getUuid());
+    $this->assertCount(1, $syncope_user_bis->getGroups());
+
+    // Clear the user.
+    $user->delete();
   }
 
   /**
    * Tests that users can be assigned global roles.
    */
   public function testUserWithGlobalRoles(): void {
-    /** @var \Drupal\user\UserInterface $user */
-    $user = $this->container->get('entity_type.manager')->getStorage('user')
-      ->create(['name' => 'jack']);
-    $user->save();
+    $user = $this->createUser('jack');
 
     // Test the client can assign the global role.
     $root_user = new SyncopeUser('', $user->label(), []);
@@ -80,6 +104,24 @@ class SyncopeUserTest extends SyncopeTestBase {
       $this->assertEmpty($groups);
       $this->getClient()->deleteUser($syncope_user->getUuid());
     }
+  }
+
+  /**
+   * Creates a user.
+   *
+   * @param string $name
+   *   The name.
+   *
+   * @return \Drupal\user\UserInterface
+   *   The user.
+   */
+  protected function createUser(string $name): UserInterface {
+    /** @var \Drupal\user\UserInterface $user */
+    $user = $this->container->get('entity_type.manager')->getStorage('user')
+      ->create(['name' => $name]);
+
+    $user->save();
+    return $user;
   }
 
 }
